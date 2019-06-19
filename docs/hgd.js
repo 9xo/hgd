e260db29211cb0c3398a4b47c14a62ce108e022a
=== ?&̳¡̳†̳@ <hello@n1x.website>
2019-06-20 02:38:53 +0530

    Update hgd.js

--- docs/hgd.js
+++ docs/hgd.js
@@ -23,10 +23,10 @@ request.onload = function () {
       const h1 = document.createElement('h1');
       h1.textContent = ' at the maximus, ⛔: ' + id.max_supply;
       const h2 = document.createElement('h2');
-      h2.textContent = 'nOUR, ' + id.name + ',⇨ has now just' + id.total_supply;
+      h2.textContent = 'nOUR, ' + id.name + ' , ' + id.percent_change_7d + ' ⇨ has now just' + id.total_supply;
 
       ee = document.createElement('eeee');
-      ee.textContent = ' awor ' + id.market_cap_usd + ' vCrazy,№. » ' + id.rank + 'th this dæy!';
+      ee.textContent = id.price_BTC + ' awor ' + id.market_cap_usd + ' vCrazy,№. » ' + id.rank + 'th this dæy!';
       const p = document.createElement('p');
       id.name = id.name.substring(3, 9);
       p.textContent = `${id.name}...`;
